afef449d44633de0eafd8bb80f4ad32c835cfe63
ArsiTech <haydarmehryar@yahoo.com>
2015-02-25 08:48:47 +0330

    Ready to Demo
    Initial Launch

---
+++ ParsShahab03/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ParsShahab03
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			//bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+			//			"~/Scripts/jquery-{version}.js"));
+
+			//bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+			//			"~/Scripts/jquery.validate*"));
+
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+			//bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+			//			"~/Scripts/modernizr-*"));
+
+			//bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+			//		  "~/Scripts/bootstrap.js",
+			//		  "~/Scripts/respond.js"));
+
+			//bundles.Add(new StyleBundle("~/Content/css").Include(
+			//		  "~/Content/bootstrap.css",
+			//		  "~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/css/bootstrap.min.css",
+				"~/Content/css/bootstrap-rtl.min.css",
+				"~/Content/css/font-awesome.css",
+				"~/Content/css/animate.min.css",
+				"~/Content/css/prettyPhoto.css",
+				"~/Content/css/main.css",
+				"~/Content/css/responsive.css"
+				));
+			bundles.Add(new ScriptBundle("~/Scripts").Include(
+				"~/Scripts/js/jquery.js",
+				"~/Scripts/js/bootstrap.min.js",
+				"~/Scripts/js/jquery.prettyPhoto.js",
+				"~/Scripts/js/jquery.isotope.min.js",
+				"~/Scripts/js/main.js",
+				"~/Scripts/js/wow.min.js"
+				));
+		}
+
+	}
+}
